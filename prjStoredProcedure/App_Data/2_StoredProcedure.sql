a089c00e1f95c0ef541305e2b0c388bb5494b531
ismeihchen <ismeihchen@gmail.com>
2019-01-27 00:00:23 +0800

    ASP.NET-MVC-StoredProcedure綀習
    新增註解

--- prjStoredProcedure/App_Data/2_StoredProcedure.sql
+++ prjStoredProcedure/App_Data/2_StoredProcedure.sql
@@ -29,7 +29,7 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
-
+--讀取所有員工資料
 CREATE PROCEDURE [dbo].[usp_GetAllEmployee]
 AS
 BEGIN
@@ -87,7 +87,7 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
-
+-- 依員工編號查詢員工資料
 CREATE PROCEDURE [dbo].[usp_GetEmployeeByEmpID]
 (
     @EmployeeId int 
